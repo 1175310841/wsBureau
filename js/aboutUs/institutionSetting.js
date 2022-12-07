a0d0c75f6c4372ef681432117b6409117e9f61dc
陈大冬 <1175310841@qq.com>
2022-12-08 00:17:21 +0800

    初始化

--- js/aboutUs/institutionSetting.js
+++ js/aboutUs/institutionSetting.js
@@ -1,18 +1,45 @@
-const { createApp } = Vue;
+let imgIndex = 0; //显示的图片索引
+let process;
+let img = document.querySelectorAll(".dept-small-img");
+let bigImage = document.querySelector(".dept-big-img");
 
-createApp({
-  data() {
-    return {
-      maskShow: false,
-    };
-  },
-  mounted() {},
-  methods: {
-    openMask() {
-      this.maskShow = true;
-    },
-    closeMask() {
-      this.maskShow = false;
-    },
-  },
-}).mount("#app");
+function showImg(index) {
+  // 小图恢复默认状态
+  for (let i = 0; i < img.length; i++) {
+    img[i].className = "dept-small-img ";
+  }
+  // 当前位移的小图片
+  img[index].className = "dept-small-img  moveSmallImg";
+  // 大图片显示
+  bigImage.style.backgroundImage = `url('../../imgs/aboutUs/${index + 1}.png')`;
+}
+// 开始轮播
+function startToggle() {
+  process = setInterval(function () {
+    imgIndex = imgIndex + 1;
+    if (imgIndex == 4) {
+      imgIndex = 0;
+    }
+    showImg(imgIndex);
+  }, 3000);
+}
+
+// 定义小图的点击响应事件
+for (let i = 0; i < img.length; i++) {
+  img[i].onclick = function () {
+    clearInterval(process);
+    imgIndex = i;
+    showImg(i);
+    startToggle();
+  };
+}
+
+// 当鼠标放置在大图上面时，停止轮播
+bigImage.onmouseover = function () {
+  clearInterval(process);
+};
+bigImage.onmouseout = function () {
+  startToggle();
+};
+
+startToggle();
